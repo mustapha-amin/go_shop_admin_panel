7a3183a1203f1a41cc3a894e726b00e7a9e36c5b
mustapha amin <mustaphaamin2003@gmail.com>
2023-05-21 17:33:09 -0700

    Now corresponds with bootstrap 4 breakpoints

--- lib/responsive.dart
+++ lib/responsive.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
 
-class Responsive extends StatelessWidget {
-  final Widget mobile;
-  final Widget desktop;
-  const Responsive({
-    super.key,
-    required this.mobile,
-    required this.desktop,
-  });
+class Responsive {
+
+  
 
   static bool isMobile(BuildContext context) =>
       MediaQuery.of(context).size.width < 850;
@@ -15,13 +10,4 @@ class Responsive extends StatelessWidget {
   static bool isDesktop(BuildContext context) =>
       MediaQuery.of(context).size.width >= 1100;
 
-  @override
-  Widget build(BuildContext context) {
-    final Size _size = MediaQuery.of(context).size;
-    if (_size.width >= 1100) {
-      return desktop;
-    } else {
-      return mobile;
-    }
-  }
 }
